9f0d1355d57b0e601a4e4aa166821f248a55ac5f
GeoffreyBraire <braire2@yahoo.fr>
2018-06-15 15:51:59 +0200

    Ajout de la partie Users

---
+++ src/main/java/com/esgi/annualproject/HotelShareApplication/HotelShareApplication.java
@@ -0,0 +1,15 @@
+package com.esgi.annualproject.HotelShareApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HotelShareApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelShareApplication.class, args);
+	}
+
+}
